09106b9e1dbef6e3c6dc99f184bdf1b1f6c8ad66
YEJIN <yejon9858@gmail.com>
2021-08-06 04:28:36 +0900

    Add : week4 서버

---
+++ Sparta/Web/homeworks/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.dbhomework
+
+
+## HTML 화면 보여주기
+@app.route('/')
+def homework():
+    return render_template('index.html')
+
+
+# 주문하기(POST) API
+@app.route('/order', methods=['POST'])
+def save_order():
+    name_receive = request.form['name_give']
+    num_receive = request.form['num_give']
+    address_receive = request.form['address_give']
+    phone_receive = request.form['phone_give']
+    doc = {
+        'name' :name_receive,
+        'num' : num_receive,
+        'address' : address_receive,
+        'phone' : phone_receive
+    }
+    db.orders.insert_one(doc)
+    return jsonify({'msg': '주문이 저장되었습니다.'})
+
+
+# 주문 목록보기(Read) API
+@app.route('/order', methods=['GET'])
+def view_orders():
+    orders = list(db.orders.find({}, {'_id' : False}))
+    return jsonify({'all_orders': orders})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
