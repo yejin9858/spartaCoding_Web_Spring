082460491206513e537951397be0658d557e53e0
YEJIN <yejon9858@gmail.com>
2021-09-18 23:59:13 +0900

    Add : week04 init

---
+++ Sparta/Spring/week04/src/test/java/sparta/week04/Week04ApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.week04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
