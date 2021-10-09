2b66bf54eee3d248e32517b1c373d77c9c8bc784
YEJIN <yejon9858@gmail.com>
2021-10-10 02:45:30 +0900

    Add : Week04 검색 기능 구현

--- Sparta/Spring/week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
+++ Sparta/Spring/week04/src/main/java/com/sparta/week04/utils/NaverShopSearch.java
@@ -1,12 +1,20 @@
 package com.sparta.week04.utils;
 
+import com.sparta.week04.models.ItemDto;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.http.*;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class NaverShopSearch {
 
 
-    private String search() {
+    public String search(String query) {
         RestTemplate rest = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-Naver-Client-Id", "HGZB3zR7MhA25Txr8TDT");
@@ -14,7 +22,7 @@ public class NaverShopSearch {
         String body = "";
 
         HttpEntity<String> requestEntity = new HttpEntity<String>(body, headers);
-        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=아디다스", HttpMethod.GET, requestEntity, String.class);
+        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=" + query, HttpMethod.GET, requestEntity, String.class);
         HttpStatus httpStatus = responseEntity.getStatusCode();
         int status = httpStatus.value();
         String response = responseEntity.getBody();
@@ -24,9 +32,23 @@ public class NaverShopSearch {
         return response;
     }
 
+    public List<ItemDto> fromJSONtoItems(String result) {
+        JSONObject rjson = new JSONObject(result);
+        JSONArray items  = rjson.getJSONArray("items");
+        List<ItemDto> itemDtoList = new ArrayList<>();
+        for (int i=0; i<items.length(); i++) {
+            JSONObject itemJson = items.getJSONObject(i);
+            System.out.println(itemJson);
+            ItemDto itemDto = new ItemDto(itemJson);
+            itemDtoList.add(itemDto);
+        }
+        return itemDtoList;
+    }
 
     public static void main(String[] args){
         NaverShopSearch naverShopSearch = new NaverShopSearch();
-        naverShopSearch.search();
+        String result = naverShopSearch.search("아이맥");
     }
+
+
 }
